811750dd0eaad60210f854d5224a3464d2fc07f4
Luis Fernandez Alvarez <luis.fernandez.alvarez@cern.ch>
2013-10-10 17:34:25 +0200

    Added 'feature' defined resource. Old openstack_hyper_v::base::windows_feature

--- manifests/add_feature.pp
+++
@@ -1,7 +0,0 @@
-
-  define add_windows_feature(){
-    exec { "ps_add_feature_${name}":
-      path    => $::path,
-      command => "powershell.exe -executionpolicy remotesigned -Command Add-WindowsFeature -Name ${name}",
-    }
-  }
