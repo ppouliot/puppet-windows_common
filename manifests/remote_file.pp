ee8ccbd9cae73fe97a957aa789c9d7548450d02c
Luis Fernandez Alvarez <luis.fernandez.alvarez@cern.ch>
2013-10-15 11:08:37 +0200

    Added defined resource remote_file

---
+++ manifests/remote_file.pp
@@ -0,0 +1,30 @@
+# == Define: windows_common::remote_file
+#
+# It downloads a remote file to a local path.
+#
+# === Parameters
+#
+# [*source*]
+#   The remote location of the file. The current version only supports HTTP.
+# [*destination*]
+#   The full path on the local filesystem.
+#
+# === Examples
+#
+#  windows_common::remote_file { 'my-remote-file':
+#    source      => "http://192.168.1.1/remote_file.ext",
+#    destination => 'C:\Folder\local_copy.ext',
+#  }
+#
+# === Authors
+#
+# === Copyright
+#
+define windows_common::remote_file($source, $destination){
+  exec{ $name:
+    command  => "(new-object Net.WebClient).DownloadFile(\'${source}\',\'${destination}\')",
+    creates  => $destination,
+    unless   => "exit !(Test-Path -Path '${destination}')",
+    provider => powershell,
+  }
+}
