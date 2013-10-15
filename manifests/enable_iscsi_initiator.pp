5eb6eee17a60e3b72c22167d9dc180df4e422a63
Peter Pouliot <ppouliot@gmail.com>
2013-10-15 10:15:12 -0400

    removing legacy files

--- manifests/enable_iscsi_initiator.pp
+++
@@ -1,7 +0,0 @@
-class windows_common::enable_iscsi_initiator {
-    notify { "Making sure the ISCSI Initiator Starts on Boot": }
-    service { 'MSiSCSI':
-      ensure => 'running',
-      enable => true,
-    }
-}
