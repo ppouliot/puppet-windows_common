40f3c7e101e86fe61996efa7b377d584b568b1ee
Nick Meier <nmeier@microsoft.com>
2013-10-16 17:45:41 -0600

    Added unless attribute to check if machine is domain joined, and removed similar code from the PowerShell script.

--- manifests/domain/joindomain.pp
+++ manifests/domain/joindomain.pp
@@ -1,31 +1,26 @@
-#############################
+###############################################################
 #
 # Windows_Common::Domain::joindomain
 #
 # Description:
-#    Definition to join a Windows machine to a Windows domain.
+#    Definition to join a Windows machine to a Windows domain,
+#    unless the machine is already a member of the specified
+#    domain.
 #
 # Example usage:
-#    Windows_Common::Domain::joindomain': { 'testdom.net':
+#    windows_common::domain::joindomain: { 'testdom.net':
 #      user_name => 'domainAdminUsername',
-#      password => 'domainAdminPassword',
+#      password  => 'domainAdminPassword',
 #    }
 #
-#############################
+#
+###############################################################
 
 define windows_common::domain::joindomain ( $user_name, $password ) {
 
   if $::osfamily == 'windows' {
 
     $code = " \
-      \$cs=Get-WMIObject Win32_ComputerSystem; \
-      if (-not \$?) { \
-        Write-Error 'Error - Unable to create Win32_ComputerSystem object'; \
-        exit 10; \
-      } \
-      if (\$cs.Domain -eq \$domain_name) { \
-        exit 0; \
-      } \
       \$secStr=ConvertTo-SecureString '${password}' -AsPlainText -Force; \
       if (-not \$?) { \
         write-error 'Error: Unable to convert password string to a secure string'; \
@@ -47,10 +42,11 @@ define windows_common::domain::joindomain ( $user_name, $password ) {
     # Use the Josh Cooper PowerShell provider
     #
     exec { 'join_domain':
-      group   => Administrators,
-      command  => "$code",
-      provider => powershell,
+      group     => Administrators,
+      command   => "$code",
+      provider  => powershell,
       logoutput => true,
+      unless    => "if ((Get-WMIObject Win32_ComputerSystem).Domain -ne '$name') { exit 1 }",
     }
   }
 }
