45bf6e9446c7a921528aba08727809b2ed602f90
Vijay Tripathi <vijayt.124@gmail.com>
2013-10-17 14:37:10 -0700

    Code for Vexasoft_Cmdlet_Library_x64 Package
    Code for Vexasoft_Cmdlet_Library_x64 Package, it is required for RDP

--- manifests/configuration/rdp.pp
+++ manifests/configuration/rdp.pp
@@ -7,8 +7,30 @@
 # Actions:
 #
 
-class windows_common::configuration::rdp{
-
+class windows_common::configuration::rdp(
+$url       = undef,
+){
+  if $url == undef {
+	  if $::architecture == 'x64' {
+		$url_real = 'http://cdn.shopify.com/s/files/1/0206/6424/files/Vexasoft_Cmdlet_Library_x64.msi'
+	  } else {
+		$url_real = 'http://cdn.shopify.com/s/files/1/0206/6424/files/Vexasoft_Cmdlet_Library_x86.msi'
+	  }
+  }
+  
+  windows_common::remote_file{ 'Vexasoft_Cmdlet_Lib':
+      source      => $url_real,
+      destination => "${::temp}\\Vexasoft_Cmdlet_Lib.msi",
+      before      => Package['Vexasoft_Cmdlet_Library_x64.msi'],
+    }
+	
+  package { 'Vexasoft_Cmdlet_Library_x64.msi':
+    ensure          => installed,
+    source          => $url_real,
+    install_options => ['/quiet', '/passive'],
+	provider => 'windows',
+  }
+  
   exec { 'enable_rdp_connection':
     command => 'powershell.exe -executionpolicy remotesigned -Command (Get-WmiObject win32_TerminalServiceSetting -Namespace root\\cimv2\\TerminalServices).SetAllowTSConnections(1)',
   }
