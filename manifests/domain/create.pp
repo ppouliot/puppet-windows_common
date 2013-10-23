2ad797bff5263bee741aa03dee0d929aed5023ea
Peter Pouliot <ppouliot@gmail.com>
2013-10-23 15:59:41 -0400

    fixing modulefile

--- manifests/domain/create.pp
+++ manifests/domain/create.pp
@@ -15,8 +15,9 @@ class windows_common::domain::create{
   #
   define create_ad_domain ( $::domain_name, $::netbios_name, $::domain_user, $::domain_passwd,){
     exec {'create_active_directory_domain':
-      command => "powershell.exe -executionpolicy remotesigned -Command Install-ADDSForest -CreateDNSDelegation:\$false -DatabasePath \"${::windir}\\NTDS\" --DomainMode \"Win2012\" -DomainName \"${::domain_name}\" -DomainNetbiosName \"${::netbiso_name}\" -ForestMode \"Win2012\" -InstallDNS:\$true -LogPath \"${::windir}\\NTDS\" -NoRebootOnCompletion:\$false -SysVolPath \"${::windir}\\SYSVOL\" -Force:\$true",
-      unless  => 'powershell.exe -executionpolicy remotesigned -Command Import-Module ADDSDeployment',
+      command => "Install-ADDSForest -CreateDNSDelegation:\$false -DatabasePath \"${::windir}\\NTDS\" --DomainMode \"Win2012\" -DomainName \"${::domain_name}\" -DomainNetbiosName \"${::netbiso_name}\" -ForestMode \"Win2012\" -InstallDNS:\$true -LogPath \"${::windir}\\NTDS\" -NoRebootOnCompletion:\$false -SysVolPath \"${::windir}\\SYSVOL\" -Force:\$true",
+      unless  => 'Import-Module ADDSDeployment',
+      provider => powershell,  
     }
   }
 
