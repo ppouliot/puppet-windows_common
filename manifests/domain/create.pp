7ae57e14fe8a2d33f8e60b52c9f1b82192671ebc
Vijay Tripathi <vijayt.124@gmail.com>
2013-10-23 17:58:34 -0700

    Removing username parameter, using onlyif
    removing $domain_username parameter as only admin password is required
    for domain join command.
    Adding provide=powershell
    changing unless to onlyif

--- manifests/domain/create.pp
+++ manifests/domain/create.pp
@@ -13,10 +13,11 @@ class windows_common::domain::create{
   # Define: windows::commands::create_ad_domain
   # Create an Active Directory Domain
   #
-  define create_ad_domain ( $::domain_name, $::netbios_name, $::domain_user, $::domain_passwd,){
+  define create_ad_domain ( $domain_name, $netbios_name, $admin_passwd,){
     exec {'create_active_directory_domain':
-      command => "powershell.exe -executionpolicy remotesigned -Command Install-ADDSForest -CreateDNSDelegation:\$false -DatabasePath \"${::windir}\\NTDS\" --DomainMode \"Win2012\" -DomainName \"${::domain_name}\" -DomainNetbiosName \"${::netbiso_name}\" -ForestMode \"Win2012\" -InstallDNS:\$true -LogPath \"${::windir}\\NTDS\" -NoRebootOnCompletion:\$false -SysVolPath \"${::windir}\\SYSVOL\" -Force:\$true",
-      unless  => 'powershell.exe -executionpolicy remotesigned -Command Import-Module ADDSDeployment',
+      command => "Install-ADDSForest -CreateDNSDelegation:\$false -DatabasePath \"${::windir}\\NTDS\" -DomainMode \"Win2012\" -DomainName \"${domain_name}\" -DomainNetbiosName \"${netbios_name}\" -ForestMode \"Win2012\" -InstallDNS:\$true -LogPath \"${::windir}\\NTDS\" -SafeModeAdministratorPassword (convertto-securestring \"${admin_passwd}\" -asplaintext -force) -NoRebootOnCompletion:\$false -SysVolPath \"${::windir}\\SYSVOL\" -Force:\$true",
+      onlyif => 'Import-Module ADDSDeployment',
+	  provider	=> powershell,
     }
   }
 
