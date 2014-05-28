b3cb6989fcdbd419b50799f06e96b1a482ba1e2e
primeministerp <primeministerpete@gmail.com>
2014-05-28 11:26:51 -0400

    fixing powershell to create a storage pool

--- manifests/configuration/create_storage_pool.pp
+++ manifests/configuration/create_storage_pool.pp
@@ -1,4 +1,4 @@
 class windows_common::configuration::create_storage_pool ($storage_pool) {
-  exec     => "New-StoragePool -StorageSubSystemID Get-StorageSubSystem.UniqueId -FriendlyName ${storage_pool} -PhysicalDisks (Get-PhysicalDisk -CanPool $true)",
+  exec     => "New-StoragePool -FriendlyName ${storage_pool} -StorageSubsystemFriendlyName "Storage Spaces*" -PhysicalDisks (Get-PhysicalDisk -CanPool $true)",
   provider => powershell,
 }
